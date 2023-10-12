1ad366322b4116af2fbea1f164528c8b10b229b2
Matheus M. Prado <102251484+Thuizin@users.noreply.github.com>
2023-10-11 21:47:14 -0300

    configurando menu e finalizando ajustes

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var menu = document.getElementById("menu");
+var menu2 = document.getElementById("menu2");
+var active = document.getElementById("active");
+menu.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menu2.style.display = 'block';
+    active.style.display = 'block';
+});
+menu2.addEventListener('click', () => {
+    menu2.style.display = 'none';
+    menu.style.display = 'block';
+    active.style.display = 'none';
+});
